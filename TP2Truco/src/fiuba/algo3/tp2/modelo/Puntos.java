f246e59af2cf4af33585806babeea7d8e66f9b8c
pabliyo <pablofeliz1989@gmail.com>
2015-11-10 04:32:33 +0100

    constructor de puntos

--- TP2Truco/src/fiuba/algo3/tp2/modelo/Puntos.java
+++ TP2Truco/src/fiuba/algo3/tp2/modelo/Puntos.java
@@ -1,5 +1,13 @@
 package fiuba.algo3.tp2.modelo;
 
 public class Puntos {
-
+    
+	private int puntajeUno;
+	private int puntajeDos;
+	
+	public void Puntos (){
+		this.puntajeUno=0;
+		this.puntajeDos=0;
+	}
+	
 }
