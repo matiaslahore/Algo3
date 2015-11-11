987cd4a56a48a2e1b3abec2d44abcbcffb7e84d5
matiaslahore <mati_ml04@hotmail.com>
2015-11-11 16:44:42 -0300

    Agregando funcionalidades

--- TP2Truco/src/fiuba/algo3/tp2/modelo/Envido.java
+++ TP2Truco/src/fiuba/algo3/tp2/modelo/Envido.java
@@ -46,8 +46,8 @@ public class Envido {
 	
 	//Falta que Jugador sepa supar los puntos del envido
 	private void sumarCartasEnvido(){
-		ArrayList<Jugador> jugadoresEquipo1 = this.equipoCantor.obtenerJugadores();
-		ArrayList<Jugador> jugadoresEquipo2 = this.equipoAceptor.obtenerJugadores();
+		ArrayList<Jugador> jugadoresEquipo1 = this.equipoCantor.obtenerJugadores(); //Una vez creada la clase juez, pedir a
+		ArrayList<Jugador> jugadoresEquipo2 = this.equipoAceptor.obtenerJugadores();//juez quien es la mano y ponerlo como eq1
 		int puntajeMaximo = 0;
 		for(int i=1; i<= 3; i++){
 			if(jugadoresEquipo1.get(i).obtenerPuntosEnvido() > puntajeMaximo){
--- TP2Truco/src/fiuba/algo3/tp2/modelo/Truco.java
+++ TP2Truco/src/fiuba/algo3/tp2/modelo/Truco.java
@@ -1,22 +1,65 @@
 package fiuba.algo3.tp2.modelo;
 
+import java.util.ArrayList;
+
 public class Truco {
-	private Jugador EquipoCantor;
-	private Jugador EquipoAceptor;
+	private Equipo equipoCantor;
+	private Equipo equipoAceptor;
+	private Equipo ganador;
+	private int puntosGanados;
+	private int cantidadDetrucosCantados;
 	
 	public Truco(){
+		this.equipoAceptor = this.equipoCantor = this.ganador = null;
+		this.puntosGanados = 0;
+		this.cantidadDetrucosCantados = 1;
 	}
 	
-	public void cantarTruco(Jugador jugadorQueCanta){
-		this.EquipoCantor = jugadorQueCanta;
+	public void cantarTruco(Equipo equiporQueCanta){
+		this.equipoCantor = equiporQueCanta;
 	}
 	
-	public void querer(Jugador jugadorQueQuiere){
-		this.EquipoAceptor = jugadorQueQuiere;
+	public void querer(Equipo equipoQueQuiere){
+		if(equipoQueQuiere == this.equipoCantor)
+			return;
+		this.equipoAceptor = equipoQueQuiere;
+		this.sumarCartasTruco();
+		this.puntosGanados = (this.cantidadDetrucosCantados);
 	}
 	
-	public String obtenerGanador(Jugador jugadorQueQuiere){
-		return "";
+	public Equipo obtenerGanador(Equipo equiporQueQuiere){
+		return(this.ganador);
 	}
+	
+	public int obtenerPuntajeGanador(){
+		return this.puntosGanados;
+	}
+	
+	public void noQuerer(Equipo equipoQueNoQuiere){
+		if(equipoQueNoQuiere == this.equipoCantor)
+			return;
+		this.ganador = this.equipoCantor;
+		this.puntosGanados = this.cantidadDetrucosCantados;		
+	}
+	
+	//Falta que Jugador sepa sumar los puntos del truco
+		private void sumarCartasTruco(){
+			ArrayList<Jugador> jugadoresEquipo1 = this.equipoCantor.obtenerJugadores(); //Una vez creada la clase juez, pedir a
+			ArrayList<Jugador> jugadoresEquipo2 = this.equipoAceptor.obtenerJugadores();//juez quien es la mano y ponerlo como eq1
+			int puntajeMaximo = 0;
+			for(int i=1; i<= 3; i++){
+				if(jugadoresEquipo1.get(i).obtenerPuntosTruco() > puntajeMaximo){
+					puntajeMaximo = jugadoresEquipo1.get(i).obtenerPuntosTruco();
+					this.ganador = this.equipoCantor;
+				}
+				if(jugadoresEquipo2.get(i).obtenerPuntosTruco() > puntajeMaximo){
+					puntajeMaximo = jugadoresEquipo1.get(i).obtenerPuntosTruco();
+					this.ganador = this.equipoAceptor;
+				}
+			}
+			if(puntajeMaximo == 0){
+				this.ganador = this.equipoCantor;
+			}
+		}
 	
 }
