af7ddd369d90670801ff24d8bd515350d8195490
NicolasLercari <niko06_14@hotmail.com>
2015-11-11 17:06:42 -0300

    agregue metodo en jugador

---
+++ TP2Truco/test/fiuba/algo3/tp2/modelo/PruebaJugadorTest.java
@@ -0,0 +1,64 @@
+package fiuba.algo3.tp2.modelo;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PruebaJugadorTest {
+	
+	private Jugador unJugador;
+	
+	@Before
+	public void inicializado(){
+		
+		unJugador = new Jugador("Nicolas");
+	}
+	
+	@Test
+	public void pruebaCalcularEnvidoDeCartasSinFlor(){
+		
+		unJugador.recibirCartas(new Carta(new Oro(), 3), new Carta(new Oro(), 7), new Carta(new Basto(),10));
+		
+		assertEquals(30,unJugador.obtenerPuntosEnvido());
+	}
+	
+	@Test
+	public void pruebaCalcularEnvidoConCartasDeDistintoPalo(){		
+	
+		unJugador.recibirCartas(new Carta(new Oro(), 3), new Carta(new Basto(), 7), new Carta(new Espada(),10));
+		
+		assertEquals(7, unJugador.obtenerPuntosEnvido());
+	}
+	
+	@Test
+	public void pruebaCalcularEnvidoConTresCartasDelMismoPalo(){		
+	
+		unJugador.recibirCartas(new Carta(new Oro(), 3), new Carta(new Oro(), 7), new Carta(new Oro(),2));
+		
+		assertEquals(30, unJugador.obtenerPuntosEnvido());
+	}
+	
+	@Test
+	public void pruebaCalcularEnvidoConTresCartasNegrasDelMismoPalo(){
+		
+
+		unJugador.recibirCartas(new Carta(new Oro(), 10), new Carta(new Oro(), 11), new Carta(new Oro(),12));
+		
+		assertEquals(20, unJugador.obtenerPuntosEnvido());
+		
+	}
+	
+	@Test
+	public void pruebaCalcularEnvidoConTresCartasNegrasDeDistintoPalo(){
+		
+		unJugador.recibirCartas(new Carta(new Basto(), 10), new Carta(new Oro(), 11), new Carta(new Espada(),12));
+		
+		assertEquals(0, unJugador.obtenerPuntosEnvido());
+		
+	}
+}
