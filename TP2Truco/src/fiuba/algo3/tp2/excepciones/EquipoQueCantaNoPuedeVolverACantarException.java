1f05daf1bc54d22978bd62c3993eaa01089290fb
matiaslahore <mati_ml04@hotmail.com>
2015-11-23 09:50:58 -0300

    Agregue excepciones a envido y cree un paquete para guardar todas las excepciones

---
+++ TP2Truco/src/fiuba/algo3/tp2/excepciones/EquipoQueCantaNoPuedeVolverACantarException.java
@@ -0,0 +1,5 @@
+package fiuba.algo3.tp2.excepciones;
+
+public class EquipoQueCantaNoPuedeVolverACantarException extends Exception {
+
+}
--- TP2Truco/src/fiuba/algo3/tp2/modelo/Envido.java
+++ TP2Truco/src/fiuba/algo3/tp2/modelo/Envido.java
@@ -2,6 +2,11 @@ package fiuba.algo3.tp2.modelo;
 
 import java.util.ArrayList;
 
+import fiuba.algo3.tp2.excepciones.EquipoQueCantaNoPuedeNoQuererElCantoException;
+import fiuba.algo3.tp2.excepciones.EquipoQueCantaNoPuedeQuererElCantoException;
+import fiuba.algo3.tp2.excepciones.EquipoQueCantaNoPuedeVolverACantar;
+import fiuba.algo3.tp2.excepciones.EquipoQueCantaNoPuedeVolverACantarException;
+import fiuba.algo3.tp2.excepciones.NoSePuedeCantarMasDeDosEnvidosException;
 import fiuba.algo3.tp2.modeloJugador.Jugador;
 
 public class Envido {
@@ -10,29 +15,33 @@ public class Envido {
 	private String equipoGanador;
 	private int puntosGanados;
 	private int cantidadDeEnvidosCantados;
-
+	private static final int cantidadDeEnvidosPosibles = 2;
+	
 	public Envido(){
 		this.cantidadDeEnvidosCantados = this.puntosGanados = 0;
 		this.equipoAceptor = this.equipoCantor = this.equipoGanador = "";
 	}
 
-	public void cantarEnvido(Jugador jugadorQueCanta){
-		if((equipoCantor != jugadorQueCanta.obtenerEquipoQuePertenece()) && (this.cantidadDeEnvidosCantados < 2)){ 
-			this.equipoCantor = jugadorQueCanta.obtenerEquipoQuePertenece();
-			this.cantidadDeEnvidosCantados++;
-		}
+	public void cantarEnvido(Jugador jugadorQueCanta) throws NoSePuedeCantarMasDeDosEnvidosException, EquipoQueCantaNoPuedeVolverACantarException{
+		if(equipoCantor == jugadorQueCanta.obtenerEquipoQuePertenece())
+			throw new EquipoQueCantaNoPuedeVolverACantarException();
+	    if(this.cantidadDeEnvidosCantados < cantidadDeEnvidosPosibles)
+			throw new NoSePuedeCantarMasDeDosEnvidosException();
+		this.equipoCantor = jugadorQueCanta.obtenerEquipoQuePertenece();
+		this.cantidadDeEnvidosCantados++;
+	
 	}
 
-	public void querer(Jugador jugadorQueQuiere){
+	public void querer(Jugador jugadorQueQuiere)throws EquipoQueCantaNoPuedeQuererElCantoException{
 		if(jugadorQueQuiere.obtenerEquipoQuePertenece() == this.equipoCantor)
-			return;
+			throw new EquipoQueCantaNoPuedeQuererElCantoException();
 		this.equipoAceptor = jugadorQueQuiere.obtenerEquipoQuePertenece();
 		this.puntosGanados = (2 * this.cantidadDeEnvidosCantados);
 	}
 
-	public void noQuerer(Jugador jugadorQueNoQuiere){
+	public void noQuerer(Jugador jugadorQueNoQuiere)throws EquipoQueCantaNoPuedeNoQuererElCantoException{
 		if(jugadorQueNoQuiere.obtenerEquipoQuePertenece() == this.equipoCantor)
-			return;
+			throw new EquipoQueCantaNoPuedeNoQuererElCantoException();
 		this.equipoGanador = this.equipoCantor;
 		this.puntosGanados = this.cantidadDeEnvidosCantados;		
 	}
