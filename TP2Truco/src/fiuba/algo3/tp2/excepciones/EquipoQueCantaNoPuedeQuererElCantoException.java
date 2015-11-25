fdab06050305d262d95bc8c5c9cd56a5fe65cbb9
matiaslahore <mati_ml04@hotmail.com>
2015-11-25 11:39:39 -0300

    excepciones

--- TP2Truco/src/fiuba/algo3/tp2/excepciones/EquipoQueCantaNoPuedeQuererElCantoException.java
+++ TP2Truco/src/fiuba/algo3/tp2/excepciones/EquipoQueCantaNoPuedeQuererElCantoException.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.tp2.excepciones;
 
-public class EquipoQueCantaNoPuedeQuererElCantoException extends Exception {
+public class EquipoQueCantaNoPuedeQuererElCantoException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
 }
