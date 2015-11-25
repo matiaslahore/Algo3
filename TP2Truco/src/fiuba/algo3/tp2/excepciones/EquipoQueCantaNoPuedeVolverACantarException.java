a7d9482a1d59767b02cab3a9ad72caa34a9a22b8
matiaslahore <mati_ml04@hotmail.com>
2015-11-25 11:40:00 -0300

    excepciones

--- TP2Truco/src/fiuba/algo3/tp2/excepciones/EquipoQueCantaNoPuedeVolverACantarException.java
+++ TP2Truco/src/fiuba/algo3/tp2/excepciones/EquipoQueCantaNoPuedeVolverACantarException.java
@@ -1,5 +1,6 @@
 package fiuba.algo3.tp2.excepciones;
 
-public class EquipoQueCantaNoPuedeVolverACantarException extends Exception {
+public class EquipoQueCantaNoPuedeVolverACantarException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
 }
