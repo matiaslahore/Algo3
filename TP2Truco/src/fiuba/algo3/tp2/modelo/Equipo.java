c569e2edf4ad9895663f32a790fa832835b0f3de
Matias Bazzana <matu.bazzana@gmail.com>
2015-12-01 17:18:33 -0300

    borrado de metodos al pedo

--- TP2Truco/src/fiuba/algo3/tp2/modelo/Equipo.java
+++ TP2Truco/src/fiuba/algo3/tp2/modelo/Equipo.java
@@ -19,15 +19,6 @@ public class Equipo {
 		this.nombreEquipo = nombreDelEquipo;
 	}
 	
-	/*public void cargarJugadores(LinkedList<String> nombreJugadores) {
-		Iterator<String> itr = nombreJugadores.iterator();
-		while(itr.hasNext()) {
-			String nombre = (String) itr.next();
-			Jugador newJugador = new Jugador(nombre,refMesa);
-			newJugador.cargarEquipo(nombreEquipo);
-			jugadores.add(newJugador);
-		}
-	}*/
 	public void cargarJugadores(String nombre) {
 		Jugador newJugador = new Humano(nombre,refMesa,this);
 		jugadores.add(newJugador);
@@ -38,27 +29,6 @@ public class Equipo {
 		Jugador nuevoJugador = new IA(nombre, refMesa, this);
 		jugadores.add(nuevoJugador);
 	}
-	
-
-	/*public void recibirCartas() {
-		for (Jugador unJugador : jugadores){
-			TipoDeCartas c1 = refMesa.repartirCarta();
-			TipoDeCartas c2 = refMesa.repartirCarta();
-			TipoDeCartas c3 = refMesa.repartirCarta();
-			
-		 	unJugador.recibirCartas(c1,c2,c3);
-		}
-	}
-
-	public Jugador obtenerJugador(String nombreJugador) {	
-		Iterator<Jugador> itr = jugadores.iterator();
-		while(itr.hasNext()) {
-			Jugador actual = (Jugador) itr.next();
-			if (nombreJugador == actual.obtenerNombre()) return actual;
-		}
-		return new Jugador("ERROR",refMesa,this); //aca hay q tirar una excepcion xq no encontro al jugador
-		//puse eso para q no me joda eclipse
-	}*/
 
 	public ArrayList<Jugador> obtenerJugadores(){
 		return(this.jugadores);
