fbb2153816cb754645505fb29167270630672057
pabliyo <pablofeliz1989@gmail.com>
2015-12-03 19:10:57 +0100

    boton truco con comportamiento

--- TP2Truco/src/fiuba/algo3/tp2/vista/BotonTrucoEventHandler.java
+++ TP2Truco/src/fiuba/algo3/tp2/vista/BotonTrucoEventHandler.java
@@ -1,21 +1,33 @@
 package fiuba.algo3.tp2.vista;
 
-import fiuba.algo3.tp2.modeloJugador.Jugador;
+import fiuba.algo3.tp2.excepciones.CantoInvalidoException;
+import fiuba.algo3.tp2.modelo.PartidaDeTruco;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class BotonTrucoEventHandler implements EventHandler<ActionEvent>{
 	
-	private Jugador jugador;
+	private PartidaDeTruco partida;
+	private Label etiqueta;
 	
 	//constructor
-	public BotonTrucoEventHandler (Jugador jugador){
-		this.jugador = jugador;
+	public BotonTrucoEventHandler (PartidaDeTruco partida, Label etiqueta){
+		this.partida=partida;
+		this.etiqueta = etiqueta;
 	}
 	
 	@Override
 	public void handle(ActionEvent actionEvent){
-		this.jugador.cantarTruco();
+		try{
+		this.partida.cantarTruco();
+		} catch (CantoInvalidoException FaltaEnvidoError){
+			etiqueta.setText("NO PUEDE CANTAR TRUCO");
+	        etiqueta.setTextFill(Color.web("#FF0000"));
+		}
+		etiqueta.setText("RONDA EN ESTADO: TRUCO");
+        etiqueta.setTextFill(Color.web("#FF0000"));
 	}
 
 }
