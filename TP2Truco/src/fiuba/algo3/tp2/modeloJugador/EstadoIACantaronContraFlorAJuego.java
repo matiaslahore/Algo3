4b9865fa4b5182c0f24ece3faaea3c0bac468d78
matiaslahore <mati_ml04@hotmail.com>
2015-12-05 18:21:45 -0300

    Agregando inteligencia al canto de la flor de la IA

--- TP2Truco/src/fiuba/algo3/tp2/modeloJugador/EstadoIACantaronContraFlorAJuego.java
+++ TP2Truco/src/fiuba/algo3/tp2/modeloJugador/EstadoIACantaronContraFlorAJuego.java
@@ -4,8 +4,15 @@ public class EstadoIACantaronContraFlorAJuego extends EstadoIA {
 
 	@Override
 	public void manejador(IA jugadorIA) {
-		// TODO Auto-generated method stub
-
+		if(jugadorIA.obtenerPuntosFlor() >= 37){
+			jugadorIA.cantarContraFlorAJuego();
+			return;
+		}
+		if(jugadorIA.obtenerPuntosFlor() >= 35){
+			jugadorIA.quiero();
+			return;
+		}
+		jugadorIA.noQuiero();
 	}
 
 }
