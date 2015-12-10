d6f006abaad384bb390ae93aa4611f35f4c0f5fa
pabliyo <pablofeliz1989@gmail.com>
2015-12-10 07:55:33 +0100

    arregle un test

--- TP2Truco/test/fiuba/algo3/tp2/visualizador/ImagenesCartaTest.java
+++ TP2Truco/test/fiuba/algo3/tp2/visualizador/ImagenesCartaTest.java
@@ -14,9 +14,13 @@ public class ImagenesCartaTest {
 	
 	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/c2.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Dos De Copa"));
 	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/c3.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Tres De Copa"));
-	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/c1.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Uno De Copa"));
-	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/c4.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Cuatro De Copa"));	
-	 	
+	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/c4.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Cuatro De Copa"));
+	
+	//basto
+	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/b3.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Tres De Basto"));
+	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/b4.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Cuatro De Basto"));
+	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/b5.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Cinco De Basto"));
+	Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/b6.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Seis De Basto"));	
 	 Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/b7.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Siete De Basto"));
 	 Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/b12.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Doce De Basto"));
 	 Assert.assertEquals("/fiuba/algo3/tp2/vista/imagenes/b11.jpg", this.imagenesCarta.obtenerDireccionDeCarta("Once De Basto"));
