2115ec150e9df84c906a9a929aa9bc7fe790fea3
matiaslahore <mati_ml04@hotmail.com>
2015-12-10 12:51:00 -0300

    cambiando nombre atributos de puntos

--- TP2Truco/src/fiuba/algo3/tp2/modelo/Puntos.java
+++ TP2Truco/src/fiuba/algo3/tp2/modelo/Puntos.java
@@ -9,50 +9,50 @@ import java.util.Set;
 
 public class Puntos {
     
-	private Map<Equipo, Integer> mapa;
+	private Map<Equipo, Integer> puntosDelJuego;
 	
 	public Puntos(Equipo equipoUno, Equipo equipoDos){
-		mapa = new HashMap<Equipo, Integer>();
-		mapa.put(equipoUno, 0);
-		mapa.put(equipoDos, 0);
+		puntosDelJuego = new HashMap<Equipo, Integer>();
+		puntosDelJuego.put(equipoUno, 0);
+		puntosDelJuego.put(equipoDos, 0);
 	}
 	
 	public void anotarPuntos(Equipo equipo, int cantidad){
-		int puntos = mapa.get(equipo);
+		int puntos = puntosDelJuego.get(equipo);
 		puntos = puntos + cantidad;
-		mapa.put(equipo,puntos);
+		puntosDelJuego.put(equipo,puntos);
 	}
 	
 	public int getPuntaje(Equipo equipo){
-		return this.mapa.get(equipo);
+		return this.puntosDelJuego.get(equipo);
 	}
 	
 	public int getPuntaje(String equipo){
-		Set<Equipo> set = mapa.keySet();
+		Set<Equipo> set = puntosDelJuego.keySet();
 	
 		Iterator<Equipo> itrK = set.iterator();
 		
 		while (itrK.hasNext()){
 			Equipo actual = itrK.next();
-			if (equipo == actual.obtenerNombre()) return this.mapa.get(actual);
+			if (equipo == actual.obtenerNombre()) return this.puntosDelJuego.get(actual);
 		}
 		return 0;
 	}
 
 	public int puntosDeLaFalta(Equipo equipo) {
-		int puntaje = mapa.get(equipo);
+		int puntaje = puntosDelJuego.get(equipo);
 		
 		if (puntaje>15) return (30-puntaje);
 		else return (15-puntaje);
 	}
 
 	public int getPuntajeMaximo() {
-		Collection<Integer> coleccion = mapa.values();
+		Collection<Integer> coleccion = puntosDelJuego.values();
 		return Collections.max(coleccion);
 	}
 
 	public boolean rangoEstaEnPicaPica() {
-		Collection<Integer> coleccion = mapa.values();
+		Collection<Integer> coleccion = puntosDelJuego.values();
 		int max = Collections.max(coleccion);
 		if (max>5 && max<25){
 			return true;
@@ -60,8 +60,8 @@ public class Puntos {
 	}
 
 	public void imprimirResultados() {
-		Collection<Integer> coleccion = mapa.values();
-		Set<Equipo> set = mapa.keySet();
+		Collection<Integer> coleccion = puntosDelJuego.values();
+		Set<Equipo> set = puntosDelJuego.keySet();
 	
 		Iterator<Equipo> itrK = set.iterator();
 		Iterator<Integer> itrV = coleccion.iterator();
