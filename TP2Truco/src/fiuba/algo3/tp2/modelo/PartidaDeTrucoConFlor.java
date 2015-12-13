d513145fbdf2c001a7227b24371aee1d7259c9c3
matiaslahore <mati_ml04@hotmail.com>
2015-12-13 17:36:00 -0300

    quitando bugs

--- TP2Truco/src/fiuba/algo3/tp2/modelo/PartidaDeTrucoConFlor.java
+++ TP2Truco/src/fiuba/algo3/tp2/modelo/PartidaDeTrucoConFlor.java
@@ -4,8 +4,6 @@ public class PartidaDeTrucoConFlor extends PartidaDeTruco{
 
 	public PartidaDeTrucoConFlor(String nombreEq1, String nombreEq2) {
 		super(nombreEq1, nombreEq2);
-		
-		this.mesa.instanciarJuez(equipoUno, equipoDos);
 	}
 
 	@Override
